53dc0141042633d48e98e0a329258662561b10d4
Murder-Nova <sebastian.correa1@correo.icesi.edu.co>
2019-09-25 22:53:28 -0500

    Script added to save data between scenes. Summary script needs to be fixed.

--- NetFooball/Assets/Scripts/GUI/Summary.cs
+++ NetFooball/Assets/Scripts/GUI/Summary.cs
@@ -5,11 +5,35 @@ using UnityEngine.UI;
 
 public class Summary : MonoBehaviour
 {
+    public Text firstPlayerName;
+    public Text secondPlayerName;
+    public Text firstPlayerScore;
+    public Text secondPlayerScore;
+    public Text winnerName;
+    public GlobalControl result;
 
-    public Text score1;
-    public Text score2;
-    public Text winner;
+    void Awake()
+    {
+        firstPlayerName = result.Instance.player1.GetComponent<Player>().Name;
+        secondPlayerName = result.Instance.player2.GetComponent<Bot>().name;
+        firstPlayerScore = result.Instance.ball.GetComponent<Ball>().score1;
+        secondPlayerScore = result.Instance.ball.GetComponent<Ball>().score2;
+        setWinner();
+    }
 
-
-
+    private void setWinner()
+    {
+        if (Int32.Parse(firstPlayerScore.text) > Int32.Parse(secondPlayerScore.text))
+        {
+            winnerName.text = firstPlayerName.text;
+        }
+        else if (Int32.Parse(firstPlayerScore) < Int32.Parse(secondPlayerScore))
+        {
+            winnerName.text = secondPlayerName.text;
+        }
+        else
+        {
+            winnerName.text = "Draw";
+        }
+    }
 }
---
+++ NetFooball/Assets/Scripts/GlobalControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalControl : MonoBehaviour
+{
+    public static GlobalControl Instance;
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject ball;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        player1 = GlobalControl.Instance.player1;
+        player2 = GlobalControl.Instance.player2;
+        ball = GlobalControl.Instance.ball;
+    }
+
+    public void saveData()
+    {
+        GlobalControl.Instance.player1 = player1;
+        GlobalControl.Instance.player2 = player2;
+        GlobalControl.Instance.ball = ball;
+    }
+}
