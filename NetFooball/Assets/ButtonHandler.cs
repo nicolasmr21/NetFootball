3df7cab6da3f61d865bf47e2d87b8262a172f156
Iván Camilo Goez Palacio <camilogoez2015@hotmail.com>
2019-10-23 01:59:37 -0500

    Video button

---
+++ NetFooball/Assets/ButtonHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void OnClic(){
+        Process proceso = new Process();
+        proceso.StartInfo.FileName = @"C:\Users\CAMILO\Desktop\Claves.txt";
+        proceso.Start();
+   }
+}
